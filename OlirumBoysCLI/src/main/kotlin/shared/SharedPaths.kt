81fce7d73f057fc3aba91c408fa1a32de479ef09
Matheus Ha <matheus.ha@gmail.com>
2021-10-17 14:11:50 +0900

    Revert "Update config"
    This reverts commit a4cad9974b9ebc826822f9cab464b6edd2ec522f.

--- OlirumBoysCLI/src/main/kotlin/shared/SharedPaths.kt
+++ OlirumBoysCLI/src/main/kotlin/shared/SharedPaths.kt
@@ -5,8 +5,8 @@ class SharedPaths
     companion object {
         //Insert in host and port accordingly to your dbeaver/VM. The same will apply to database, username and password.
         //try using .gitignore on this file
-        val JDBC_CONNECTION_STRING = "192.168.56.101"
-        val USERNAME = "root"
-        val PASSWORD = "root"
+        val JDBC_CONNECTION_STRING = "jdbc:mysql://{host}:{port}/olirum_boys"
+        val USERNAME = ""
+        val PASSWORD = ""
     }
 }
