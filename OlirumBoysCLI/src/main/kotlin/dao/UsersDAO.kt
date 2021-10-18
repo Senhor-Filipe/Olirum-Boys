801fba35d2a8423e65e4a1753900d3ea927f69f2
Senhor-Filipe <79452743+Senhor-Filipe@users.noreply.github.com>
2021-10-17 22:38:15 -0300

    Create UsersDAO.kt

---
+++ OlirumBoysCLI/src/main/kotlin/dao/UsersDAO.kt
@@ -0,0 +1,177 @@
+package dao
+
+import models.User
+
+class UserDAO: GenericDAO {
+    override fun idSearch(id: Int): Any
+    {
+        var users = mutableListOf<User>()
+
+        try {
+            val connection = ConnectionDAO()
+            val resultSet = connection.executeQuery("SELECT * FROM Users WHERE user_id = ${id};")
+            while (resultSet?.next()!!)
+            {
+                users.add(
+                    User(
+                        resultSet.getInt("user_id"),
+                        resultSet.getString("user_name"),
+                        resultSet.getString("user_pwd"),
+                        resultSet.getString("user_level")
+                    )
+                )
+            }
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+
+        return users
+    }
+
+    fun nameSearch(user_name: String): Any
+    {
+        var users = mutableListOf<User>()
+
+        try {
+            val connection = ConnectionDAO()
+            val resultSet = connection.executeQuery("SELECT * FROM Users WHERE user_name = \"${user_name}\";")
+            while (resultSet?.next()!!)
+            {
+                users.add(
+                    User(
+                        resultSet.getInt("user_id"),
+                        resultSet.getString("user_name"),
+                        resultSet.getString("user_pwd"),
+                        resultSet.getString("user_level")
+                    )
+                )
+            }
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+
+        return users
+    }
+
+    override fun getAll(): List<Any>
+    {
+        var users = mutableListOf<User>()
+        try {
+            val connection = ConnectionDAO()
+            val resultSet = connection.executeQuery("SELECT * FROM olirum_boys.Users")
+
+            while (resultSet?.next()!!)
+            {
+                users.add(
+                    User(
+                        resultSet.getInt("user_id"),
+                        resultSet.getString("user_name"),
+                        resultSet.getString("user_pwd"),
+                        resultSet.getString("user_level")
+                    )
+                )
+            }
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+        return user
+    }
+
+    override fun insert(obj: Any)
+    {
+        val user = obj as user
+        if (!possibleDuplicateDetector(user.user_name))
+        {
+            try {
+                val connection = ConnectionDAO()
+                val preparedStatement = connection.getPreparedStatement(
+                    """
+                INSERT INTO olirum_boys.Users
+                (user_name, user_pwd, user_level)
+                VALUES (?, ?, ?);
+                """.trimMargin()
+                )
+                preparedStatement?.setString(1, user.user_name)
+                preparedStatement?.setString(2, user.user_pwd)
+                preparedStatement?.setString(3, user.user_level)
+                preparedStatement?.executeUpdate()
+                connection.commit()
+                connection.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        else
+        {
+            println("User already exists in the database.")
+            println(nameSearch(user.user_name))
+            println("\n")
+        }
+    }
+
+    override fun update(obj: Any)
+    {
+        val user = obj as User
+        try {
+            val connection = ConnectionDAO()
+            val preparedStatement = connection.getPreparedStatement("""
+                UPDATE olirum_boys.Users
+                SET user_name = ?, user_pwd = ?, user_level = ?
+                WHERE user_id = ?;
+                """.trimMargin())
+            preparedStatement?.setString(1, user.user_name)
+            preparedStatement?.setString(2, user.user_pwd)
+            preparedStatement?.setString(3, user.user_level)
+            preparedStatement?.setInt(4, user.user_id)
+            preparedStatement?.executeUpdate()
+            connection.commit()
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun delete(id: Int)
+    {
+        try {
+            val connection = ConnectionDAO()
+            val preparedStatement = connection.getPreparedStatement("DELETE FROM olirum_boys.Users WHERE user_id = ${id}")
+            preparedStatement?.executeUpdate()
+            connection.commit()
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /*
+        This function will detect, when inserting a new User object to the database, if said user already exists
+      in the database.
+     */
+    fun possibleDuplicateDetector(user_name: String): Boolean
+    {
+        var possibleDuplicates = mutableListOf<String>()
+        try {
+            val connection = ConnectionDAO()
+            val resultSet = connection.executeQuery("""
+                SELECT *
+                FROM olirum_boys.Users
+                WHERE user_name = "${user_name}"
+                """.trimMargin())
+
+            while (resultSet?.next()!!)
+            {
+                possibleDuplicates.add(resultSet.getString("user_name"))
+            }
+            connection.close()
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
+        if (possibleDuplicates.isEmpty())
+            return false
+        return true
+    }
+}
